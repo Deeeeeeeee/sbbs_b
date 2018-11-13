8fbdfa8a954dddfd90fa6f36f109972d022b5bbb
Deeeeeeeee <seal.de@foxmail.com>
2018-11-13 09:32:20 +0800

    Mod

--- learn4_test.go
+++ learn4_test.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"regexp"
 	"testing"
+	"time"
 )
 
 // 正则表达式
@@ -114,4 +115,43 @@ func TestJson(t *testing.T) {
 	enc := json.NewEncoder(os.Stdout)
 	d := map[string]int{"apple": 5, "lettuce": 7}
 	enc.Encode(d)
+}
+
+// Time
+func TestTime(t *testing.T) {
+	p := fmt.Println
+
+	now := time.Now()
+	p(now)
+
+	then := time.Date(
+		2009, 11, 17, 20, 34, 58, 651387237, time.UTC,
+	)
+	p(then)
+
+	p(then.Year())
+	p(then.Month())
+	p(then.Day())
+	p(then.Hour())
+	p(then.Minute())
+	p(then.Second())
+	p(then.Nanosecond())
+	p(then.Location())
+
+	p(then.Weekday())
+
+	p(then.Before(now))
+	p(then.After(now))
+	p(then.Equal(now))
+
+	diff := now.Sub(then)
+	p(diff)
+
+	p(diff.Hours())
+	p(diff.Minutes())
+	p(diff.Seconds())
+	p(diff.Nanoseconds())
+
+	p(then.Add(diff))
+	p(then.Add(-diff))
 }
