0560ba93addb4999594afd6adfcdbd0525a11429
Deeeeeeeee <seal.de@foxmail.com>
2018-11-14 09:32:35 +0800

    Add

--- learn4_test.go
+++ learn4_test.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha1"
 	"encoding/json"
 	"fmt"
+	"math/rand"
+	"net"
+	"net/url"
 	"os"
 	"regexp"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -154,4 +159,146 @@ func TestTime(t *testing.T) {
 
 	p(then.Add(diff))
 	p(then.Add(-diff))
+}
+
+// Epoch
+func TestEpoch(t *testing.T) {
+	now := time.Now()
+	secs := now.Unix()
+	nanos := now.UnixNano()
+	fmt.Println(now)
+
+	millis := nanos / 1000000
+	fmt.Println(secs)
+	fmt.Println(millis)
+	fmt.Println(nanos)
+
+	fmt.Println(time.Unix(secs, 0))
+	fmt.Println(time.Unix(0, nanos))
+}
+
+// 时间格式化和时间解析
+func TestTimeFormattingAndParsing(t *testing.T) {
+	p := fmt.Println
+
+	tt := time.Now()
+	p(tt.Format(time.RFC3339))
+
+	t1, e := time.Parse(
+		time.RFC3339,
+		"2012-11-01T22:08:41+00:00",
+	)
+	p(t1)
+
+	p(tt.Format("3:04PM"))
+	p(tt.Format("Mon Jan _2 15:04:05 2006"))
+	p(tt.Format("2006-01-02T15:04:05.999999-07:00"))
+	form := "3.04 PM"
+	t2, e := time.Parse(form, "8 41 PM")
+	p(t2)
+
+	fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
+		tt.Year(), tt.Month(), tt.Day(),
+		tt.Hour(), tt.Minute(), tt.Second())
+
+	ansic := "Mon Jan _2 15:04:05 2006"
+	_, e = time.Parse(ansic, "8:41PM")
+	p(e)
+}
+
+// 随机数 默认没有种子数，或者种子数相同的情况，随机数的序列都是一样的。。。
+// 要使用随机数，建议使用 crypto/rand
+func TestRandomNumnbers(t *testing.T) {
+	fmt.Print(rand.Intn(100), ",")
+	fmt.Print(rand.Intn(100))
+	fmt.Println()
+
+	fmt.Println(rand.Float64())
+
+	fmt.Print((rand.Float64()*5)+5, ",")
+	fmt.Print((rand.Float64() * 5) + 5)
+	fmt.Println()
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	fmt.Print(r1.Intn(100), ",")
+	fmt.Print(r1.Intn(100))
+	fmt.Println()
+
+	s2 := rand.NewSource(42)
+	r2 := rand.New(s2)
+	fmt.Print(r2.Intn(100), ",")
+	fmt.Print(r2.Intn(100))
+	fmt.Println()
+	s3 := rand.NewSource(42)
+	r3 := rand.New(s3)
+	fmt.Print(r3.Intn(100), ",")
+	fmt.Print(r3.Intn(100))
+	fmt.Println()
+}
+
+// 解析数字
+func TestNumberParsing(t *testing.T) {
+	f, _ := strconv.ParseFloat("1.234", 64)
+	fmt.Println(f)
+
+	i, _ := strconv.ParseInt("123", 0, 64)
+	fmt.Println(i)
+
+	d, _ := strconv.ParseInt("0x1c8", 0, 64)
+	fmt.Println(d)
+
+	u, _ := strconv.ParseUint("789", 0, 64)
+	fmt.Println(u)
+
+	k, _ := strconv.Atoi("135")
+	fmt.Println(k)
+
+	_, e := strconv.Atoi("wat")
+	fmt.Println(e)
+}
+
+// 解析url
+func TestURLParsing(t *testing.T) {
+	s := "postgres://user:pass@host.com:5432/path?k=v#f"
+
+	u, err := url.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(u.Scheme)
+
+	fmt.Println(u.User)
+	fmt.Println(u.User.Username())
+	p, _ := u.User.Password()
+	fmt.Println(p)
+
+	fmt.Println(u.Host)
+	host, port, _ := net.SplitHostPort(u.Host)
+	fmt.Println(host)
+	fmt.Println(port)
+
+	fmt.Println(u.Path)
+	fmt.Println(u.Fragment)
+
+	fmt.Println(u.RawQuery)
+	m, _ := url.ParseQuery(u.RawQuery)
+	fmt.Println(m)
+	fmt.Println(m["k"][0])
+}
+
+// sha1 hashes
+func TestSHA1Hashes(t *testing.T) {
+	s := "sha1 this string"
+
+	h := sha1.New()
+
+	h.Write([]byte(s))
+
+	bs := h.Sum(nil)
+
+	fmt.Println(s)
+	fmt.Printf("%x\n", bs)
 }
